906d2cffbdc5ed10b925aae7c8af90360bf2b0da
rukiAtty <rukshanaattygalle@gmail.com>
2023-11-01 18:41:31 +0530

    tests  updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+      version='1.0',
+      description='Wine Quality with MLOPS',
+      author='Rukshana',
+      packages = find_packages(),
+      license='MIT')
+
